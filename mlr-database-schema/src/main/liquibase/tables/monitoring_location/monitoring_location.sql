fe60e8acdaab8534ca74a342d91681a4ad121779
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2017-08-01 14:11:29 -0500

    add altitude datum and method tables, add tables folder and reorg, add roles and schema yaml files.

--- mlr-database-schema/src/main/liquibase/tables/monitoring_location/monitoring_location.sql
+++ mlr-database-schema/src/main/liquibase/tables/monitoring_location/monitoring_location.sql
@@ -1,10 +1,10 @@
 create table mlr_data.monitoring_location
 (monitoring_location_id			integer not null
 ,primary_agency_id				integer not null
-,name							character varying(50) not null
-,location_number				character varying(15)
+,name							text not null
+,location_number				text
 ,type_id						integer
-,feature_id						integer
+,feature						jsonb
 ,country_id						integer
 ,state_id						integer
 ,county_id						integer
@@ -13,10 +13,10 @@ create table mlr_data.monitoring_location
 ,national_aquifer_id			integer
 ,aquifer_id						integer
 ,create_date					timestamp
-,create_user					character varying(20)
+,create_user					text
 ,last_update_date				timestamp
-,last_update_user				character varying(20)
-,constraint monitoring_location_id_pk
+,last_update_user				text
+,constraint monitoring_location_pk
   primary key (monitoring_location_id)
 );
 alter table mlr_data.monitoring_location owner to mlr_data;
