f7feda6bad6aa128274997a4da1987ebac134592
zmoore-usgs <zmoore@usgs.gov>
2017-08-01 13:40:22 -0500

    Updated monitoring location DAO to use an interface - implementation pattern.

---
+++ mlr-data-api/src/main/java/gov/usgs/wma/dao/IMonitoringLocationDAO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.usgs.wma.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import gov.usgs.wma.model.MonitoringLocation;
+/**
+ *
+ * @author zmoore
+ */
+public interface IMonitoringLocationDAO {
+
+	//Abstract Methods
+	public List<MonitoringLocation> getMonitoringLocations(Map<String, Object> params);
+	public MonitoringLocation createMonitoringLocation(MonitoringLocation newMonitoringLocation);
+	
+	//TODO - Implement Updates
+	//public MonitoringLocation updateMonitoringLocation(MonitoringLocation updatedMonitoringLocation);
+	
+	//TODO - Implement Deletes
+	//public void deleteMonitoringLocation(MonitoringLocation monitoringLocationToDelete);
+	
+	//Default Methods -- READ
+	public default List<MonitoringLocation> getMonitoringLocations() {return getMonitoringLocations(new HashMap<String,Object>());}
+		public default MonitoringLocation getMonitoringLocationById(Long id) {
+		return getSingleMonitoringLocationByParam("id", id);
+	};
+	
+	public default MonitoringLocation getMonitoringLocationByLocationNumber(Long locationNumber) {
+		return getSingleMonitoringLocationByParam("locationNumber", locationNumber);
+	};
+	
+	public default MonitoringLocation getSingleMonitoringLocationByParam(String key, Object value) {
+		HashMap<String,Object> params = new HashMap<>();
+		MonitoringLocation returnLocation = null;
+		params.put(key, value);
+		
+		List<MonitoringLocation> retrievedLocations = getMonitoringLocations(params);
+		
+		if(retrievedLocations != null && retrievedLocations.size() > 0){
+			returnLocation = retrievedLocations.get(0);
+		}
+		
+		return returnLocation;
+	};
+	
+	//Default Methods -- Delete	
+	//public default void deleteMonitoringLocationById(Long locationNumber)
+	
+	//public default void deleteMontiroingLocationByLocationNumber(Long locationNumber)
+	
+	
+}
--- mlr-data-postgres-impl/src/main/java/gov/usgs/wma/dao/MonitoringLocationDAO.java
+++ mlr-data-postgres-impl/src/main/java/gov/usgs/wma/dao/MonitoringLocationDAO.java
@@ -1,7 +1,5 @@
 package gov.usgs.wma.dao;
 
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,14 +8,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import gov.usgs.wma.mapper.MonitoringLocationMapper;
 import gov.usgs.wma.model.MonitoringLocation;
+import gov.usgs.wma.mapper.MonitoringLocationMapper;
 
 /**
  *
  * @author zmoore
  */
-public class MonitoringLocationDAO {
+public class MonitoringLocationDAO implements IMonitoringLocationDAO {
 	private static final Logger LOG = LoggerFactory.getLogger(MonitoringLocationDAO.class);
 	
 	private SqlSessionFactory sessionFactory;
@@ -26,39 +24,22 @@ public class MonitoringLocationDAO {
 		this.sessionFactory = MLDAOFacotry.getSqlSessionFactory();
 	}
 	
-	public List<MonitoringLocation> getMonitoringLocations() {
-		return getMonitoringLocations(new HashMap<String,Object>());
-	}
-	
-	public MonitoringLocation getMonitoringLocation(String id){
-		MonitoringLocation returnLocation = null;
-		List<MonitoringLocation> monitoringLocations = null;
-		Map<String,Object> params = new HashMap<>();
-		
-		params.put("id", id);
-		
-		monitoringLocations = getMonitoringLocations(params);
-		
-		if(monitoringLocations != null){
-			if(monitoringLocations.size() > 1){
-				LOG.error("Duplicate MonitoringLocation Entities Found for ID: " + id);
-			} else if(monitoringLocations.size() == 1) {
-				returnLocation = monitoringLocations.get(0);
-			}
-		}
-		
-		return returnLocation;
-	}
-	
+	@Override
 	public List<MonitoringLocation> getMonitoringLocations(Map<String, Object> params) {
 		List<MonitoringLocation> monitoringLocations = null;
 		
 		try (SqlSession session = sessionFactory.openSession()) {
-			LOG.debug("Loading ML Entities for params", params.toString());
+			LOG.debug("Loading Monitoring Locations for params ", params.toString());
 			MonitoringLocationMapper mapper = session.getMapper(MonitoringLocationMapper.class);
 			monitoringLocations = mapper.getMonitoringLocations(params);
 		}
 		
 		return monitoringLocations;
+	}
+	
+	@Override
+	public MonitoringLocation createMonitoringLocation(MonitoringLocation newMonitoringLocation){
+		//TODO - Persist and re-fetch for return
+		return newMonitoringLocation;
 	}
 }
