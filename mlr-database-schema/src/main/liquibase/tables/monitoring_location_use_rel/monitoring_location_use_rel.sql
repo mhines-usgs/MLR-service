a107d46270599edfa4579fd330d81d831cc08e9b
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2017-08-02 09:37:07 -0500

    add monitoring_location_use_rel table to handle the site and water uses for a monitoring location.

---
+++ mlr-database-schema/src/main/liquibase/tables/monitoring_location_use_rel/monitoring_location_use_rel.sql
@@ -0,0 +1,8 @@
+create table mlr_data.monitoring_location_use_rel
+(monitoring_location_id			integer not null
+,use_id							integer not null
+,rank							integer not null constraint use_rank_ck (rank between 1 and 3)
+,constraint monitoring_location_use_uk
+  unique (monitoring_location_id, use_id)
+);
+alter table mlr_data.monitoring_location_use_rel owner to mlr_data;
