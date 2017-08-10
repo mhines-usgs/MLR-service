70360009cc0f811a45119b5206bf5bd9175a8ba2
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2017-08-10 10:46:28 -0500

    Remove database check constraints on reference tables, can add later after validation rules are solidified.

--- mlr-database-schema/src/main/liquibase/tables/altitude_datum/altitude_datum.sql
+++ mlr-database-schema/src/main/liquibase/tables/altitude_datum/altitude_datum.sql
@@ -1,6 +1,6 @@
 create table mlr_data.altitude_datum
 (altitude_datum_id		integer not null
-,code							text constraint altitude_datum_code_ck check (code in ('NGVD29', 'NAVD88'))
+,code							text
 ,name							text
 ,constraint altitude_datum_pk
   primary key (altitude_datum_id)
--- mlr-database-schema/src/main/liquibase/tables/horizontal_method/horizontal_method.sql
+++ mlr-database-schema/src/main/liquibase/tables/horizontal_method/horizontal_method.sql
@@ -1,6 +1,6 @@
 create table mlr_data.horizontal_method
 (horizontal_method_id		integer not null
-,code							text constraint horizontal_method_code_ck check (code in ('C', 'D', 'G', 'L', 'M', 'N', 'R' , 'S', 'U'))
+,code							text
 ,name							text
 ,constraint horizontal_method_pk
   primary key (horizontal_method_id)
